175f3c39cce53e824e9759a62c759dcf9f8f62d0
dshirazi13 <86160554+dshirazi13@users.noreply.github.com>
2023-05-31 10:34:27 -0400

    Update background_calc_game.py

--- background_calc_game.py
+++ background_calc_game.py
@@ -1,2 +1,23 @@
 import pygame
+import sys 
+import os 
+import time
+from pygame.locals import * 
+
+xCoorPlayer = 0 
+
+
+total = 0 
+gameRunning = true
+addSum = 0
+scalefactor = 200
+
+class calc: 
+  def refreshScore(): 
+    while gameRunning: 
+    time.sleep(.1)
+    total += yCoorPlayer/scalefactor
+    
+ 
+  
 
