6068c5f847e6eb167da5dd8610f301b3ce94f250
dshirazi13 <1mail2mailredmailbluemail@gmail.com>
2023-06-01 16:05:10 -0500

    Update background_calc_game.py

--- background_calc_game.py
+++ background_calc_game.py
@@ -13,25 +13,34 @@ import sympy as sy
 
 xCoorPlayer = 0 
 
-
+totalArcLength = 0
 totalVolume = 0 
 gameRunning = true
 scalefactor = 200
-
+updateMathTime = .1 # time inerval used to calculate the integral and the arc length 
 
 
 
 class calc: 
   def refreshScoreVolume(): 
     while gameRunning: 
-        time.sleep(.1)
+        time.sleep(updateMathTime)
         totalVolume += yCoorPlayer/scalefactor
 
   def arcLengthRefresh(): 
     while gameRunning: 
-      initYCoorPlayer = yCoorPlayer
-      time.sleep(.1)
-      finalYCoorPlayer = yCoorPlayer
+      initYCoorPlayer = yCoorPlayer # initial y value
+      time.sleep(updateMathTime)
+      finalYCoorPlayer = yCoorPlayer # final y value
+      changeX = bg_speed * updateMathTime #change in x over the time interval
+      changeY = finalYCoorPlayer - initYCoorPlayer # change in y over the time interval
+      dydx = changeY/changeX
+
+      def f(x): 
+      	return sqrt(1+dydx)
+
+      x = sy.Symbol("x")
+      totalArcLength += (sy.integrate(f(x), (x, 0, changeX)))
       
 
   
